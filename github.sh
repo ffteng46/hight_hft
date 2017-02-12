1d1798773c20fca88181feee0ead9273913a347e
ffteng46 <tff.6587523@163.com>
2017-02-12 19:55:51 +0800

    2017-02-12 19:55:51

--- github.sh
+++ github.sh
@@ -2,5 +2,5 @@
 export PATH=$PATH
 echo $PATH
 sudo git add .
-sudo git commit -m `date +%Y-%m-%d`
+sudo git commit -m "`date "+%Y-%m-%d %H:%M:%S"`"
 sudo git push https://github.com/ffteng46/hight_hft.git
