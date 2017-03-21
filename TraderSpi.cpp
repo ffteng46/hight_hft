d2ff07fc08c7e228a388c2b2b546a4ad4e03df24
ffteng46 <tff.6587523@163.com>
2017-03-22 00:11:55 +0800

    2017-03-22 00:11:54

--- TraderSpi.cpp
+++ TraderSpi.cpp
@@ -1,4 +1,4 @@
-// TraderSpi.cpp: implementation of the CTraderSpi class.
+﻿// TraderSpi.cpp: implementation of the CTraderSpi class.
 //
 //////////////////////////////////////////////////////////////////////
 
@@ -1521,7 +1521,7 @@ void CTraderSpi::tradeParaProcessTwo(){
         }
         if(bidAkdSpread >= gapToAdjust && realShortPstLimit  > realLongPstLimit){
             if(priceDownToBuy < bottomOfDownToBuy){
-                priceDownToBuy += 1;
+                //priceDownToBuy += 1;
                 //priceUpToSell += 1;
                 int tmppriceDownToBuy = priceDownToBuy;
                 tmp1 = "bidAkdSpread=" + boost::lexical_cast<string>(bidAkdSpread)+" >= " + boost::lexical_cast<string>(gapToAdjust) +
@@ -1534,7 +1534,7 @@ void CTraderSpi::tradeParaProcessTwo(){
 
         }else if(bidAkdSpread >= gapToAdjust && realShortPstLimit < realLongPstLimit){
             if(priceUpToSell > bottomOfUpToSell){
-                priceUpToSell -= 1;
+                //priceUpToSell -= 1;
                 //priceDownToBuy -= 1;
                 int tmppriceUpToSell = priceUpToSell;
                 tmp1 = "bidAkdSpread=" + boost::lexical_cast<string>(bidAkdSpread)+" >= " + boost::lexical_cast<string>(gapToAdjust) +
